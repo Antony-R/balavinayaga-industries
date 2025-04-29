60d5fa812c23d43e3dd48a8f75e746e1db0abac6
Antony-R <r.antony67@gmail.com>
2025-04-29 23:38:53 +0530

    v2.0

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactusService } from '../dbservices/contactus.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-us',
@@ -15,8 +16,10 @@ export class ContactUsComponent implements OnInit {
   email: string = 'balavinayagaindustries@gmail.com';
   message: string = 'You can send your resume to the above mail id if you are interested in a job in our company.';
   location: string = 'https://maps.app.goo.gl/sJdewMHM99DUnWzk7';
+  embedLocation: string = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.9098587242815!2d80.0851339!3d12.977616999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52f5f9b6124389%3A0xfcbd41717191991f!2sBalavinayaga%20Industries!5e0!3m2!1sen!2sin!4v1745689493162!5m2!1sen!2sin';
+  safeLocationUrl: SafeResourceUrl | undefined;
 
-  constructor(private contactService: ContactusService) { }
+  constructor(private contactService: ContactusService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.contactService.getContactus().subscribe(contactUs => {
@@ -27,6 +30,8 @@ export class ContactUsComponent implements OnInit {
       this.email = contactUs[0].email;
       this.message = contactUs[0].message;
       this.location = contactUs[0].location;
+      this.embedLocation = contactUs[0].embedLocation;
+      this.safeLocationUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.embedLocation);
     })
   }
 
